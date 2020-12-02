b4f86482e1a9f169ffcc3b2412e3ecae70dfac4e
Gabriel <gabriel@pop-os.localdomain>
2020-12-02 01:07:16 -0300

    Automatizado

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo apt install postgresql postgresql-contrib -y
+sudo -u postgres psql -c "CREATE USER cloud WITH PASSWORD 'cloud';"
+sudo -u postgres createdb -O cloud tasks
+sudo sh -c "echo listen_addresses=\'*\' >> /etc/postgresql/10/main/postgresql.conf"
+sudo sh -c "echo host all all 0.0.0.0/0 trust >> /etc/postgresql/10/main/pg_hba.conf"
+sudo ufw allow 5432/tcp
+sudo systemctl restart postgresql
